fa37093d41724771c7b8af11b4796e60af084716
GHNalk2016 <18423839+GHNalk2016@users.noreply.github.com>
2020-06-03 15:24:25 +0530

    Update main.meta.js

--- main.meta.js
+++ main.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name						Styles: Upwork.com Message Centre
-// @version						0.2.3
-// @description				Clear some of the elements from the message center of Upwork.com website.
-// @author						GHNalk2016
-// @include						*://*.upwork.com/messages/rooms/*
-// @require						http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @grant							GM_addStyle
+// @name					Styles: Upwork.com Message Center
+// @version					0.2.4
+// @description				Clear some of the elements from Upwork.com message center.
+// @author					GHNalk2016
+// @include					*://*.upwork.com/messages/rooms/*
+// @require					http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @grant					GM_addStyle
 // @updateURL				https://gist.githubusercontent.com/GHNalk2016/cdf096b4a5387eb3269f55d6912bad30/raw/main.meta.js
-// @downloadURL			https://gist.githubusercontent.com/GHNalk2016/cdf096b4a5387eb3269f55d6912bad30/raw/main.user.js
+// @downloadURL				https://gist.githubusercontent.com/GHNalk2016/cdf096b4a5387eb3269f55d6912bad30/raw/main.user.js
 // ==/UserScript==
