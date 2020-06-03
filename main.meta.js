75a8eda1aa9cc041f561f464c62db3e260cbf2a0
GHNalk2016 <rwnfrnnd+github@gmail.com>
2020-06-03 16:09:35 +0530

    Update

--- main.meta.js
+++ main.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name					Styles: Upwork.com Message Center
-// @version					0.2.5
+// @version					0.2.6
 // @description				Clear some of the elements from Upwork.com message center.
 // @author					GHNalk2016
 // @include					*://*.upwork.com/messages/rooms/*
