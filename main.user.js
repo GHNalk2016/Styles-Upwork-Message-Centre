4fdba78da0c914c7acff4759c50c538bdc9d0866
GHNalk2016 <rwnfrnnd+github@gmail.com>
2020-06-03 16:27:07 +0530

    -

--- main.user.js
+++ main.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name					Styles: Upwork.com Message Center
-// @version					0.2.6
+// @version					0.2.8
 // @description				Clear some of the elements from Upwork.com message center.
 // @author					GHNalk2016
 // @include					*://*.upwork.com/messages/rooms/*
 // @require					http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @grant					GM_addStyle
-// @updateURL				https://gist.githubusercontent.com/GHNalk2016/cdf096b4a5387eb3269f55d6912bad30/raw/main.meta.js
-// @downloadURL				https://gist.githubusercontent.com/GHNalk2016/cdf096b4a5387eb3269f55d6912bad30/raw/main.user.js
+// @updateURL				https://raw.githubusercontent.com/GHNalk2016/Styles-Upwork-Message-Centre/master/main.user.js
+// @downloadURL				https://raw.githubusercontent.com/GHNalk2016/Styles-Upwork-Message-Centre/master/main.user.js
 // ==/UserScript==
 
 addGlobalStyle('#up-s-top-nav-container { display: none; }'); // On/off main header.
